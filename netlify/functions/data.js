4bb6e2942478cf2415ff0093318de5c6d243fd8c
OI-BIO2023 <oi@biologik.it>
2025-07-01 11:56:28 +0200

    Update data.js

--- netlify/functions/data.js
+++ netlify/functions/data.js
@@ -1,4 +1,4 @@
-const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, QueryCommand } = require("@aws-sdk/client-dynamodb");
 const { unmarshall } = require("@aws-sdk/util-dynamodb");
 
 const client = new DynamoDBClient({
@@ -11,21 +11,46 @@ const client = new DynamoDBClient({
 
 exports.handler = async function (event, context) {
   try {
-    const params = {
-      TableName: "MQTT_KT",   // dein Tabellenname
-      Limit: 1000,            // nicht zu groß starten
+    const tableName = "MQTT_KT";
+
+    // Parameter aus Querystring auslesen
+    const params = event.queryStringParameters || {};
+    const ident = params.ident || "Solos"; // fallback
+    const start = params.start;
+    const end = params.end;
+
+    if (!start || !end) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: "start and end parameters required" }),
+      };
+    }
+
+    const queryParams = {
+      TableName: tableName,
+      KeyConditionExpression: "#ident = :ident AND #minute BETWEEN :start AND :end",
+      ExpressionAttributeNames: {
+        "#ident": "ident",
+        "#minute": "minute"
+      },
+      ExpressionAttributeValues: {
+        ":ident": { S: ident },
+        ":start": { S: start },
+        ":end": { S: end }
+      }
     };
 
-    const command = new ScanCommand(params);
+    const command = new QueryCommand(queryParams);
     const data = await client.send(command);
 
-    const items = data.Items.map((item) => unmarshall(item));
+    const items = data.Items.map(item => unmarshall(item));
 
     return {
       statusCode: 200,
       body: JSON.stringify(items),
       headers: { "Content-Type": "application/json" },
     };
+
   } catch (err) {
     console.error(err);
     return {
